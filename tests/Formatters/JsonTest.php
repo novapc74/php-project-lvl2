308919ecfd1d916d050670c765dad03448df6186
Makarov Vyacheslav <novapc@ukr.net>
2021-07-10 15:15:34 +0300

    add tests for Json Formatter

--- tests/Formatters/JsonTest.php
+++ tests/Formatters/JsonTest.php
@@ -1,33 +1,40 @@
 <?php
 
-// namespace Differ\Phpunit\Tests\JsonTest;
+namespace Differ\Phpunit\Tests\JsonTest;
 
-// use PHPUnit\Framework\TestCase;
-// use function Differ\Formatters\Json\displayJson;
-// use function Differ\Formatters\Json\iter;
+use PHPUnit\Framework\TestCase;
+use function Differ\Formatters\Json\displayJson;
+use function Differ\Formatters\Json\iter;
 
-// class JsonTest extends TestCase
-// {
-//     public function setUp(): void
-//     {
-//         $this->structure = '{"key":"common","type":"replace","oldValue":"oldValue","newValue":"newValue"}';
+class JsonTest extends TestCase
+{
+    public function setUp(): void
+    {
+        $this->structure = '[{"key":"common","type":"nested","children":[]}]';
 
-//         $this->arrTest = [
-//             'key' => 'common',
-//             'type' => 'nested',
-//             'oldValue' => '',
-//             'newValue' => '',
-//             'children' => [[
-//                 'key' => 'follow',
-//                 'type' => 'removed',
-//                 'oldValue' => "200"],
-//                 ['key' => 'test',
-//                 'type' => 'added',
-//                 'newValue' => 'true']]
-//             ];
-//     }
-//     public function testIter()
-//     {
-//         $this->assertEquals($this->structure, iter($this->arrTest));
-//     }
-// }
+        $this->arrTest = [[
+            'key' => 'common',
+            'type' => 'nested',
+            'oldValue' => '',
+            'newValue' => '',
+            'children' => [
+            ]]];
+    }
+    public function testIter()
+    {
+        $this->assertEquals($this->structure, iter($this->arrTest));
+    }
+    public function testDysplayJson()
+    {
+        $this->structure = '{"type":"root","children":[{"key":"common","type":"nested","children":[]}]}';
+
+        $this->arrTest = [[
+            'key' => 'common',
+            'type' => 'nested',
+            'oldValue' => '',
+            'newValue' => '',
+            'children' => [
+            ]]];
+        $this->assertEquals($this->structure, displayJson($this->arrTest));
+    }
+}
