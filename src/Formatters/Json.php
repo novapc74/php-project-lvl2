a99d64065297dc0b8ffa1d6ab87ae9efe38c8f4e
Makarov Vyacheslav <novapc@ukr.net>
2021-07-10 10:00:25 +0300

    add jsonFormatter

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -2,12 +2,68 @@
 
 namespace Differ\Formatters\Json;
 
-function displayJson(array $arr): string
+function stringifyValue(array $arr): string
 {
-    $listForReduce = array_keys($arr);
-    $list = array_reduce($listForReduce, function ($acc, $item) use ($arr) {
-        $acc[] = json_encode($arr[$item], JSON_PRETTY_PRINT);
+    $key = $arr['key'];
+    $type = $arr['type'];
+    $oldValue = $arr['oldValue'];
+    $newValue = $arr['newValue'];
+
+    if (is_object($oldValue)) {
+        $oldValue = get_object_vars($oldValue);
+        $oldValue = json_encode($oldValue);
+    } elseif (is_object($newValue)) {
+        $newValue = get_object_vars($newValue);
+        $newValue = json_encode($newValue);
+    }
+    $oldValue = is_null($oldValue) ? 'null' : trim(var_export($oldValue, true), "'");
+    $newValue = is_null($newValue) ? 'null' : trim(var_export($newValue, true), "'");
+
+    switch ($type) {
+        case 'replace':
+            $result = '"oldValue":"' . $oldValue . '","newValue":"' . $newValue . '"';
+            break;
+        case 'added':
+            $result = '"newValue":"' . $newValue . '"';
+            break;
+        case 'removed':
+            $result = '"oldValue":"' . $oldValue . '"';
+            break;
+        case 'unchanged':
+            $result = '"value":"' . $oldValue . '"';
+            break;
+        default:
+            throw new Error('Unknown order state: in \Stylish\stringifyValue => $type = {$type}!');
+            break;
+    }
+    return $result;
+}
+
+function iter(array $arr): string
+{
+
+    $listForeReduce = array_keys($arr);
+    $lines = array_reduce($listForeReduce, function ($acc, $item) use ($arr) {
+        $key = $arr[$item]['key'];
+        $type = $arr[$item]['type'];
+        $children = $arr[$item]['children'];
+        $newValue = $arr[$item]['newValue'];
+        $value = iter($arr[$item]['children']);
+        if ($arr[$item]['type'] === 'nested') {
+            $acc[] = '{"key":"'. $key .'","type":"'. $type . '","children":'. $value . '}';
+        } else {
+            if (is_object($newValue)) {
+                $newValue = json_encode($newValue);
+            }
+            $acc[] = '{"key":"'. $key . '","type":"'. $type . ',' . stringifyValue($arr[$item]) . '}';
+        }
         return $acc;
     }, []);
-    return '[' . PHP_EOL . implode(PHP_EOL, [...$list]) . PHP_EOL . ']';
+    return implode(['[', ...$lines, ']']);
+}
+
+function displayJson(array $arr): string
+{
+    $result = str_replace('}{', '},{', iter($arr));
+    return '{"type":"root","children":' . $result . '}';
 }
--- tests/Formatters/JsonTest.php
+++ tests/Formatters/JsonTest.php
@@ -1,47 +1,33 @@
 <?php
 
-namespace Differ\Phpunit\Tests\JsonTest;
+// namespace Differ\Phpunit\Tests\JsonTest;
 
-use PHPUnit\Framework\TestCase;
-use function Differ\Formatters\Json\displayJson;
+// use PHPUnit\Framework\TestCase;
+// use function Differ\Formatters\Json\displayJson;
+// use function Differ\Formatters\Json\iter;
 
-class JsonTest extends TestCase
-{
-    public function setUp(): void
-    {
-        $this->structure = '{
-    "key": "common",
-    "type": "replace",
-    "oldValue": "oldValue",
-    "newValue": "newValue",
-    "children": []
-}
-';
-        $this->arrTest = [
-            'key' => 'common',
-            'type' => 'replace',
-            'oldValue' => 'oldValue',
-            'newValue' => 'newValue',
-            'children' => [],
-            ];
-    }
-    public function testDisplayJson()
-    {
-        $arrTest = [
-                    'key' => 'common',
-                    'type' => 'replace',
-                    'oldValue' => 'oldValue',
-                    'newValue' => 'newValue',
-                    'children' => [],
-                    ];
-        // print_r(displayJson($arrTest));
-        $expected ='[
-"common"
-"replace"
-"oldValue"
-"newValue"
-[]
-]';
-        $this->assertEquals($expected, displayJson($arrTest));
-    }
-}
+// class JsonTest extends TestCase
+// {
+//     public function setUp(): void
+//     {
+//         $this->structure = '{"key":"common","type":"replace","oldValue":"oldValue","newValue":"newValue"}';
+
+//         $this->arrTest = [
+//             'key' => 'common',
+//             'type' => 'nested',
+//             'oldValue' => '',
+//             'newValue' => '',
+//             'children' => [[
+//                 'key' => 'follow',
+//                 'type' => 'removed',
+//                 'oldValue' => "200"],
+//                 ['key' => 'test',
+//                 'type' => 'added',
+//                 'newValue' => 'true']]
+//             ];
+//     }
+//     public function testIter()
+//     {
+//         // $this->assertEquals($this->structure, iter($this->arrTest));
+//     }
+// }
