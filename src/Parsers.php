6ee6713c442203fcff22ef880fbd9e8219d09679
Makarov Vyacheslav <novapc@ukr.net>
2021-07-12 16:02:47 +0300

    parser fix error

--- src/Parsers.php
+++ src/Parsers.php
@@ -7,5 +7,6 @@ use Symfony\Component\Yaml\Yaml;
 function parserFile(string $path, string $typeFile): object
 {
     $fileContent = file_get_contents($path);
-    return Yaml::parse($fileContent, Yaml::PARSE_OBJECT_FOR_MAP);
+    $fileObject = Yaml::parse($fileContent, Yaml::PARSE_OBJECT_FOR_MAP);
+    return $fileObject;
 }
