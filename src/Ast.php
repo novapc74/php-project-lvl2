e924e63c3c8019d4eb9a8dd1ca5432421d1e2843
Makarov Vyacheslav <novapc@ukr.net>
2021-07-15 08:47:46 +0300

    fix sort function in AST

--- src/Ast.php
+++ src/Ast.php
@@ -2,16 +2,37 @@
 
 namespace Differ\Ast;
 
+function quickSort($array): array
+{
+    if (count($array) < 2) {
+        return $array;
+    }
+    $left = [];
+    $right = [];
+    $pivotKey = key($array);
+    $pivot = array_shift($array);
+    $q = 0;
+    $result = array_map(function ($item) use (&$left, &$right, &$q, $pivot): array {
+        if ($item < $pivot) {
+            $left[$q] = $item;
+            $q++;
+            return $left;
+        } else {
+            $right[$q] = $item;
+            $q++;
+            return $right;
+        }
+    }, $array);
+    return array_merge(quickSort($left), array($pivotKey => $pivot), quickSort($right));
+}
+
 function compareIter(object $beginObject, object $endObject): array
 {
     $keysBeginObject = array_keys(get_object_vars($beginObject));
     $keysEndObject = array_keys(get_object_vars($endObject));
     $listForMap = array_unique(array_merge($keysBeginObject, $keysEndObject));
-
-    sort($listForMap);
-    // natcasesort($listForMap);
-
-    $ast = array_map(function ($key) use ($beginObject, $endObject): array {
+    $key = quickSort($listForMap);
+    $ast = array_map(function (string $key) use ($beginObject, $endObject): array {
         $oldValue = $beginObject->$key ?? null;
         $newValue = $endObject->$key ?? null;
         if (is_object($oldValue) && is_object($newValue)) {
@@ -33,6 +54,6 @@ function compareIter(object $beginObject, object $endObject): array
             'newValue' => $newValue,
             'children' => $children ?? []
         ];
-    }, $listForMap);
+    }, $key);
     return $ast;
 }
