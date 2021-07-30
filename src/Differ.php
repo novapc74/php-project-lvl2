dcb7331218f13e7518685c76730759433700c349
Makarov Vyacheslav <novapc@ukr.net>
2021-07-30 16:28:41 +0300

    string and try

--- src/Differ.php
+++ src/Differ.php
@@ -8,14 +8,13 @@ use function Differ\Formatter\chooseFormat;
 
 function genDiff(string $firstFilePath, string $secondFilePath, string $styleString = 'stylish'): string
 {
-
-    $firstFileContent = file_get_contents($firstFilePath);
-    $extensionFirstFile = pathinfo($firstFilePath, PATHINFO_EXTENSION);
-
-    $secondFileContent = file_get_contents($secondFilePath);
-    $extensionSecondFile = pathinfo($secondFilePath, PATHINFO_EXTENSION);
-
     try {
+        $firstFileContent = (string)(file_get_contents($firstFilePath));
+        $extensionFirstFile = pathinfo($firstFilePath, PATHINFO_EXTENSION);
+
+        $secondFileContent = (string)(file_get_contents($secondFilePath));
+        $extensionSecondFile = pathinfo($secondFilePath, PATHINFO_EXTENSION);
+
         $firstObject = parserFile($firstFileContent, $extensionFirstFile);
         $secondObject = parserFile($secondFileContent, $extensionSecondFile);
         $astFormat = compareIter($firstObject, $secondObject);
