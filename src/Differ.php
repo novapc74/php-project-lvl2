411885124777a0c9c08f9370b29bd2baa6938eef
Makarov Vyacheslav <novapc@ukr.net>
2021-07-30 16:50:09 +0300

    final fix

--- src/Differ.php
+++ src/Differ.php
@@ -8,14 +8,10 @@ use function Differ\Formatter\chooseFormat;
 
 function genDiff(string $firstFilePath, string $secondFilePath, string $styleString = 'stylish'): string
 {
-    try {
         $firstFileContent = (string)(file_get_contents($firstFilePath));
         $extensionFirstFile = pathinfo($firstFilePath, PATHINFO_EXTENSION);
         $secondFileContent = (string)(file_get_contents($secondFilePath));
         $extensionSecondFile = pathinfo($secondFilePath, PATHINFO_EXTENSION);
-    } catch (\Exception $e) {
-        echo 'An exception thrown: ',  $e->getMessage(), PHP_EOL;
-    }
         $firstObject = parserFile($firstFileContent, $extensionFirstFile);
         $secondObject = parserFile($secondFileContent, $extensionSecondFile);
         $astFormat = compareIter($firstObject, $secondObject);
