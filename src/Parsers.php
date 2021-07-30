134240841cac7d064dc287c6865a0ee3e7983543
Makarov Vyacheslav <novapc@ukr.net>
2021-07-30 15:46:50 +0300

    update phpunit

--- src/Parsers.php
+++ src/Parsers.php
@@ -5,15 +5,15 @@ namespace Differ\Parsers;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Yaml\Yaml;
 
-function parserFile(string $string, string $extension = 'yaml'): object
+function parserFile(string $stringData, string $extension = 'yaml'): object
 {
     switch ($extension) {
         case 'json':
-            return json_decode($string);
+            return json_decode($stringData);
         case 'yaml':
-            return Yaml::parse($string, Yaml::PARSE_OBJECT_FOR_MAP);
+            return Yaml::parse($stringData, Yaml::PARSE_OBJECT_FOR_MAP);
         case 'yml':
-            return Yaml::parse($string, Yaml::PARSE_OBJECT_FOR_MAP);
+            return Yaml::parse($stringData, Yaml::PARSE_OBJECT_FOR_MAP);
         default:
             throw new Exception("src\Differ\Parsers non-parsed file extension");
     }
